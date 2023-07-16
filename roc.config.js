4d2983b68d4b9a269d5ded271afeb64d58e894c9
Johan le stickman <Johan_Stickman@proton.me>
2023-07-16 20:10:35 +0200

    Update roc.config.js

--- roc.config.js
+++ roc.config.js
@@ -4,9 +4,9 @@ module.exports = {
 
 	// Options de build
 	buildDir: 'build', // chemin vers le dossier de build
-	minifyHtml: true, // le code HTML sera minifié si activé, Tailwind CSS est toujours minifié
+	minifyHtml: true, // le code HTML sera minifié si activé, Tailwind CSS et le JavaScript sont toujours minifiés
 
 	// Développement
-	devPort: 3000, // process.env.PORT restera prioritaire dans tout les cas
+	devPort: 3000, // process.env.PORT restera prioritaire dans tous les cas
 	devOpenBrowser: true // ouvrir le navigateur automatiquement quand on démarre le serveur de développement
 }
