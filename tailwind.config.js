3e767d5a2eefe5db22bdddd30922780390bb9b58
Johan le stickman <Johan_Stickman@proton.me>
2023-11-27 23:58:49 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./public/**/*.{html,js}'],
-	plugins: [require('daisyui')],
-	daisyui: {
-		logs: false,
-		themes: [
-			"dark"
-		],
-	}
-}
+
